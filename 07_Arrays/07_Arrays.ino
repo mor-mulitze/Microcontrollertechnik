d62164f36e203bc92340f17116b84ff7d5df987a
Donner Bernd <bernd.donner@GBS.sabel.local>
2023-01-26 11:27:32 +0100

    Arrays Lösung. Ausgabe von Arrays.

---
+++ 07_Arrays/07_Arrays.ino
@@ -0,0 +1,50 @@
+//Array Dimensionen werden von aussen nach innen angegeben
+char text [5][4] = {
+  {'a', 'b', 'c', 'd'},  // 0->4     1->3    2->2    i->4-i
+  {'A', 'B', 'C', 'D'},
+  {'1', '2', '3', '4'},
+  {'w', 'x', 'y', 'z'},
+  {'W', 'X', 'Y', 'Z'},
+};
+
+char text2 [5][4];
+
+
+//Die Implementation dieser Funktion ist nicht ganz einfach und ist _nicht_ schulaufgabenrelevant!
+void ausgabe(char* text, uint8_t im, uint8_t jm)
+{
+  for (uint8_t i = 0; i < im; i++)
+  {
+    for (uint8_t j = 0; j < jm; j++)
+    {
+      Serial.print(*(text + i * jm + j)); //<--- Die Zeile hat es in sich!
+      Serial.print("\t");
+    }
+    Serial.println();
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Original Array: ");
+  ausgabe(text[0], 5, 4);
+
+  for (uint8_t i = 0; i < 5; i++)
+  {
+    for (uint8_t j = 0; j < 4; j++)
+    {
+      text2[4 - i][j] = text[i][j];
+    }
+  }
+  Serial.println("Modifiziertes Array: ");
+  ausgabe(text2[0], 5, 4);
+}
+
+
+
+
+void loop() {
+}
--- 08_Arrays_und_Zeiger/08_Arrays_und_Zeiger.ino
+++ 08_Arrays_und_Zeiger/08_Arrays_und_Zeiger.ino
@@ -1,18 +1,17 @@
-void setup() {
-  Serial.begin(9600);
-}
-
 //Array Dimensionen werdenvon aussen nach innen angegeben
 char text [5][4]= {
-  {'a', 'b', 'c', 'd'},
+  {'a', 'b', 'c', 'd'},  // 0->4     1->3    2->2    i->4-i
   {'A', 'B', 'C', 'D'},
   {'1', '2', '3', '4'},
   {'w', 'x', 'y', 'z'},
   {'W', 'X', 'Y', 'Z'},
 };
 
-void loop() {
-  //Zeiger und Arrays sind dasselbe
+
+void setup() {
+  Serial.begin(9600);
+
+  //Zeiger und Arrays sind ganz eng verwandt
   char* text_view1 = text[0]; //text_view1 Zeiger zeigt 
   char* text_view2 = &text[0][0]; //Adresse von text[0][0]
   Serial.print("1. Zeichen: ");
@@ -25,4 +24,9 @@ void loop() {
   Serial.println(text_view2[19]); //erster Index startet immer bei 0, 0
   Serial.print("5. Zeichen: ");
   Serial.println(*(text_view2+19)); //erster Index startet immer bei 0, 0
- }
+}
+
+
+void loop()
+{ 
+}
