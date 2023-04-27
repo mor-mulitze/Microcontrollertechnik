a3090465079e8195f5633400a654e9121a0b44fa
Donner Bernd <bernd.donner@GBS.sabel.local>
2023-04-27 10:42:37 +0200

    Umbenennung

---
+++ 025_Multiplikation_und_Shift/025_Multiplikation_und_Shift.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+  uint8_t a = 20;
+  Serial.println(a<<4);
+  Serial.println(a*2*2*2*2); //2 hoch 4
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
