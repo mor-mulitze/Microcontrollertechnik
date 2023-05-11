ad3a9f262945f3f16184aecb7cfe3d3929dfb668
Donner Bernd <bernd.donner@GBS.sabel.local>
2023-05-11 14:51:25 +0200

    Bitops1

--- Uebungsaufgabe_Bitops1/Uebungsaufgabe_Bitops1.ino
+++ Uebungsaufgabe_Bitops1/Uebungsaufgabe_Bitops1.ino
@@ -5,6 +5,18 @@ uint16_t newColor(uint8_t r, uint8_t g, uint8_t b)
 
 
 void setup() {
+  Serial.begin(9600);
+  uint16_t a = newColor(0, 0, 0);
+  Serial.println(a, BIN);
+
+  uint16_t b = newColor(255, 0, 0);
+  Serial.println(b, BIN);
+
+  Serial.println(0xF8, BIN);
+
+  uint16_t c = newColor(0, 255, 0);
+  Serial.println(c, BIN);
+
   
   // put your setup code here, to run once:
 
