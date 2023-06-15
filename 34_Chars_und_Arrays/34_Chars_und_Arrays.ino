87d528cf080aec9181e70ef84cf71c455fd65701
Donner Bernd <bernd.donner@GBS.sabel.local>
2023-06-15 12:37:46 +0200

    Arrays und Chars

---
+++ 34_Chars_und_Arrays/34_Chars_und_Arrays.ino
@@ -0,0 +1,26 @@
+void setup() {
+  char a = '~';
+  Serial.begin(9600);
+
+/*
+  Serial.println(a);
+  Serial.println((uint8_t) a);
+  Serial.println((uint8_t) a, HEX);
+*/
+
+  char b[] = ":)";
+  char c[] = "Essen";
+  int16_t d = 17;
+  Serial.println(b);
+  Serial.println(c);
+  Serial.println((uint8_t) b[3]);
+  Serial.println((uint8_t) c[6]);
+  //Adresse von b und c
+  Serial.println((uint16_t) &b);
+  Serial.println((uint16_t) &c);
+  Serial.println((uint16_t) &d);
+  
+}
+
+void loop() {
+}
