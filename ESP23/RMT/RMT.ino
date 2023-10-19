ab337ee5612796a65401e7b99c935589a1be47f9
Bernd Donner <bernd.donner@sabel.com>
2023-10-19 21:28:15 +0200

    esp32 rmt blink sketch working

--- ESP23/RMT/RMT.ino
+++ ESP23/RMT/RMT.ino
@@ -1,102 +1,52 @@
 #include <driver/rmt.h>
 #include <driver/periph_ctrl.h>
-  
+
+rmt_item32_t items[] = {
+  {{{ 32767, 1, 32767, 0 }}},
+  {{{ 32767, 1, 32767, 0 }}},
+  {{{ 32767, 1, 32767, 0 }}},
+  {{{ 0, 0, 0, 0 }}},
+};
+
 void setup() 
 {
   rmt_config_t rmt_tx;
+  rmt_tx.rmt_mode = RMT_MODE_TX;
   rmt_tx.channel = RMT_CHANNEL_0;
   rmt_tx.gpio_num = GPIO_NUM_2;                   //onboard visible LED
   rmt_tx.mem_block_num = 1;
-  rmt_tx.clk_div = 250;
+  rmt_tx.clk_div = 255;
   rmt_tx.tx_config.loop_en = false;
+  rmt_tx.tx_config.carrier_en = false;                    // \/Fix for below issue?
+
   rmt_tx.tx_config.carrier_duty_percent = 50;
   rmt_tx.tx_config.carrier_freq_hz = 38000;
   rmt_tx.tx_config.carrier_level = (rmt_carrier_level_t)1;
-  rmt_tx.tx_config.carrier_en = false;                    // \/Fix for below issue?
-//  rmt_tx.tx_config.carrier_en = RMT_TX_CARRIER_EN;      //Not in rmt driver at all?
+
   rmt_tx.tx_config.idle_level = (rmt_idle_level_t)0;
   rmt_tx.tx_config.idle_output_en = true;
-  rmt_tx.rmt_mode = RMT_MODE_TX;
-  rmt_config(&rmt_tx);
-  
-  rmt_item32_t items[3];
-  items[0].duration0 = 32767;
-  items[0].level0 = 1;
-  items[0].duration1 = 32767;
-  items[0].level1 = 0;
-  items[1].duration0 = 32767;
-  items[1].level0 = 1;
-  items[1].duration1 = 32767;
-  items[1].level1 = 0;
 
-//install system RMT driver, disable rx ringbuffer for transmitter.
-  rmt_driver_install(rmt_tx.channel, 0, 0);
-  //rmt_write_items(rmt_tx.channel, (rmt_item32_t*) items, 4, false); //replaced by??
-  rmt_fill_tx_items(rmt_tx.channel, (rmt_item32_t*) items, 4, (DR_REG_RMT_BASE + 0x800 + 0x40 * 4));
+  ESP_LOGI(TAG, "rmt_config");
+  ESP_ERROR_CHECK(rmt_config(&rmt_tx));
+  
+  //install system RMT driver, disable rx ringbuffer for transmitter.
+  ESP_LOGI(TAG, "rmt_driver_install");
+  ESP_ERROR_CHECK(rmt_driver_install(rmt_tx.channel, 0, 0));
+
+  rmt_fill_tx_items(rmt_tx.channel, (const rmt_item32_t*) items, sizeof(items) / sizeof(rmt_item32_t), 0);
   rmt_tx_start(rmt_tx.channel, true);
 
-  periph_module_enable(PERIPH_RMT_MODULE);
-  *((volatile uint32_t *) (0x3FF000C0)) |= 0x200 ;
-  *((volatile uint32_t *) (0x3FF000C4)) &= (~(0x200)) ;
+//  periph_module_enable(PERIPH_RMT_MODULE);
+//  *((volatile uint32_t *) (0x3FF000C0)) |= 0x200 ;
+//  *((volatile uint32_t *) (0x3FF000C4)) &= (~(0x200)) ;
 }
 
-void loop() 
+
+void loop()
 {
 }
 
-
-rmt_item32_t items[] = {
-    {{{ 50, 0, 50, 0 }}},
-    {{{ 2, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 1, 1, 1, 0 }}},
-    {{{ 0, 0, 0, 0 }}},
-};
-
-void app_main(void *ignore)
-{
-    rmt_config_t config;
-    config.rmt_mode = RMT_MODE_TX;
-    config.channel = RMT_CHANNEL_0;
-    config.gpio_num = 12;
-    config.mem_block_num = 1;
-    config.tx_config.carrier_en = 0;
-    // config.tx_config.loop_en = 1;
-    config.tx_config.loop_en = false;
-    config.tx_config.idle_output_en = 1;
-    config.tx_config.idle_level = 0;
-    config.clk_div = 10;
-
-    ESP_LOGI(TAG, "rmt_config");
-    ESP_ERROR_CHECK(rmt_config(&config));
-    ESP_LOGI(TAG, "rmt_driver_install");
-    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
-    int number_of_items = sizeof(items) / sizeof(rmt_item32_t);
-    ESP_LOGI(TAG, "rmt_write_items");
-    // ESP_ERROR_CHECK(rmt_write_items(RMT_CHANNEL_0, items, number_of_items, false));
-    rmt_fill_tx_items(RMT_CHANNEL_0, (const rmt_item32_t *)items, number_of_items, 0);
-    rmt_set_tx_intr_en(RMT_CHANNEL_0, false);
-    rmt_set_tx_loop_mode(RMT_CHANNEL_0, true);
-    while (1) {
-        ESP_LOGI(TAG, "Waitink");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    vTaskDelete(NULL);
-}
-
+/*
 
 from micropython import const
 from machine import mem32
@@ -121,3 +71,5 @@ print(hex(mem32[DPORT_PERIP_RST_EN_REG] & 0xffffffff))
 print(hex(mem32[MCPWM0])) # read PWM_CLK_CFG_REG (reset value = 0)
 mem32[MCPWM0] = 0x55      # change PWM_CLK_PRESCALE
 print(hex(mem32[MCPWM0])) # read PWM_CLK_CFG_REG
+
+*/
