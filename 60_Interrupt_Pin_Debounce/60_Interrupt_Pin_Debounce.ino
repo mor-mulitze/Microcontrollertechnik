ea3009644831252e976dc15492cb1f05ff6a486c
Donner Bernd <bernd.donner@GBS.sabel.local>
2023-12-08 12:39:12 +0100

    debounce somwhat working

--- 60_Interrupt_Pin_Debounce/60_Interrupt_Pin_Debounce.ino
+++ 60_Interrupt_Pin_Debounce/60_Interrupt_Pin_Debounce.ino
@@ -1,14 +1,26 @@
 const uint8_t interruptPin = 3;
 const uint8_t ledPin = 13;
+const unsigned long totzeit = 100; //0.01 Sekunden
+volatile unsigned long lastedge = 0;
+
 
 void toggleLed()
 {
-  digitalWrite(ledPin, !digitalRead(ledPin));
+  unsigned long now = millis();
+//  Serial.println("ISR");
+//  Serial.flush();
+  
+  if (now - lastedge > totzeit)
+  {
+    digitalWrite(ledPin, !digitalRead(ledPin));
+    lastedge = now;
+  }
 }
 
 
 void setup()
 {
+//  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), toggleLed, FALLING);
