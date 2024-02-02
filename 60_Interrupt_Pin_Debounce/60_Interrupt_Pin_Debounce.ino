6683047daafdc7c7383312575060b46c03b7b480
Donner Bernd <bernd.donner@GBS.sabel.local>
2024-02-02 12:07:08 +0100

    Timmer Alarm

--- 60_Interrupt_Pin_Debounce/60_Interrupt_Pin_Debounce.ino
+++ 60_Interrupt_Pin_Debounce/60_Interrupt_Pin_Debounce.ino
@@ -1,15 +1,22 @@
 const uint8_t interruptPin = 3;
 const uint8_t ledPin = 13;
-const unsigned long totzeit = 100; //0.01 Sekunden
+const unsigned long totzeit = 100; //0.1 Sekunden
 volatile unsigned long lastedge = 0;
 
+#define DEBUG
 
 void toggleLed()
 {
   unsigned long now = millis();
-//  Serial.println("ISR");
-//  Serial.flush();
-  
+
+#ifdef DEBUG
+  static uint8_t anschlag = 0;
+  Serial.print("ISR: ");
+  Serial.println(anschlag);
+  Serial.flush();
+  anschlag += 1;     //anschlag = anschlag + 1;
+#endif
+
   if (now - lastedge > totzeit)
   {
     digitalWrite(ledPin, !digitalRead(ledPin));
@@ -20,10 +27,15 @@ void toggleLed()
 
 void setup()
 {
-//  Serial.begin(9600);
+
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
+
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), toggleLed, FALLING);
+
+  attachInterrupt(digitalPinToInterrupt(interruptPin), toggleLed, CHANGE);
 }
 
 void loop() {
--- Uebungsaufgabe_Timer_Alarm/Uebungsaufgabe_Timer_Alarm.ino
+++ Uebungsaufgabe_Timer_Alarm/Uebungsaufgabe_Timer_Alarm.ino
@@ -1,11 +1,28 @@
 //1.) Zählen von 0 bis 65535 und Ausgabe
 //2.) Timer2 in den Normal Mode und Prescaler = 128
 
+volatile bool flag = false;
+
+ISR(TIMER2_OVF_vect)
+{
+  static uint16_t i = 0;
+  if (i == 2451) {
+    i = 0;
+    flag = true;
+  }
+  i++;
+}
+
+
 void setup()
 {
   Serial.begin(9600);
-  TCCR2A = 0;                              // <-------
-  TCCR2B = (1 << CS22) | (1 << CS20);      // <-------
+  cli();
+  TCCR2A = 0;                              // <------- Mode: Normal
+  TCCR2B = (1 <<  CS22) | (1 << CS20);     // <------- Prescaler: 128
+  TIMSK2 = (1 << TOIE2);                   // <------- Interrupt wird bei Timer Überlauf ausgelöst
+  sei();
+
 /*
          0000 0100
     +    0000 0001
@@ -25,7 +42,11 @@ void loop()
   vielArbeit++;
   Serial.print("Main Loop Arbeit: ");
   Serial.println(vielArbeit);
-  Serial.print("TCNT2 - Register: "); // <------
-  uint8_t a = TCNT2;                  // <------
-  Serial.println(a);                  // <------
+  if (flag) {
+    Serial.println("Game Over!!!");
+    flag = false;
+  }
+//  Serial.print("TCNT2 - Register: "); // <------
+//  uint8_t a = TCNT2;                  // <------
+//  Serial.println(a);                  // <------
 }
