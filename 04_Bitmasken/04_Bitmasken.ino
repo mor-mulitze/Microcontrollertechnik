9caf5a6e674d579e4a6ba3f746c497eaa36e8659
Bernd Donner <bernd.donner@sabel.com>
2024-06-09 20:59:44 +0200

    Update 04_Bitmasken.ino
    Code cleanup in Bitmasken

--- 04_Bitmasken/04_Bitmasken.ino
+++ 04_Bitmasken/04_Bitmasken.ino
@@ -1,15 +1,13 @@
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   uint8_t zahl     = 0b11111100;
   uint8_t bitmaske = 0b00010000;
 
   Serial.println(zahl & bitmaske, BIN);
-  // put your setup code here, to run once:
+ }
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
 }
